52858a3de222f4ace26504892645e65498cde726
Quinn Daamen <Quinndaamen08@gmail.com>
2026-01-12 11:05:36 +0100

    Added Caching for API data

--- SUE/SUE.Services/Sensors/Contracts/ISensorService.cs
+++ SUE/SUE.Services/Sensors/Contracts/ISensorService.cs
@@ -9,7 +9,6 @@ namespace SUE.Services.Sensors.Contracts
     {
         Task<SensorNodeDto> CreateSensorNodeAsync(Guid householdId, string name, string location);
         Task<IEnumerable<SensorNodeDto>> GetSensorNodesAsync(Guid householdId);
-
         Task<MeasurementDto> AddMeasurementAsync(Guid sensorNodeId, MeasurementDto measurement);
         Task<IEnumerable<MeasurementDto>> GetMeasurementsAsync(Guid sensorNodeId, DateTime? from = null, DateTime? to = null);
     }
