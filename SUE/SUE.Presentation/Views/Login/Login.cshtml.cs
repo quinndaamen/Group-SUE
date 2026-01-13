93b5f9019e46d548b4423d4617c9dd0eefc1e62e
jurrian <jurriandelacroes@gmail.com>
2026-01-13 12:57:59 +0100

    edded main page

---
+++ SUE/SUE.Presentation/Views/Login/Login.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SUE.Presentation.Views.Login;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ SUE/SUE.Presentation/Views/Register/Register.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SUE.Presentation.Views.Register;
+
+public class Register : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
