a5f139af780b149d5b1e4edec0d6b3a4508c5f7f
Quinn Daamen <Quinndaamen08@gmail.com>
2026-01-14 14:59:31 +0100

    Adding GraphPages

---
+++ SUE/SUE.Presentation/Controllers/GraphController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SUE.Presentation.Controllers;
+
+public class GraphController : Controller
+{
+    public GraphController()
+    {
+    }
+    
+    public IActionResult ElectricityGraph()
+    {
+        return View();
+    }
+
+    public IActionResult AirGraph()
+    {
+        return View();
+    }
+}
