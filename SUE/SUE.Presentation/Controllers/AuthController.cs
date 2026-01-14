0f2c58753a3c1326af0130cf5a9ce1feed9a2a20
Quinn Daamen <Quinndaamen08@gmail.com>
2026-01-14 13:06:05 +0100

    Adding new pages

---
+++ SUE/SUE.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SUE.Presentation.Controllers;
+
+public class AuthController : Controller
+{
+    // GET
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
