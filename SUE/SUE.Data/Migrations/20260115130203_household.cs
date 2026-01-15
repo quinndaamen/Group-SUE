f0843ac3492ccc49883fd8dfaf7cec0435de6cc8
Quinn Daamen <Quinndaamen08@gmail.com>
2026-01-15 14:02:44 +0100

    migration household

---
+++ SUE/SUE.Data/Migrations/20260115130203_household.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SUE.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class household : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RegionCode",
+                table: "Households",
+                newName: "P1ID");
+
+            migrationBuilder.AddColumn<string>(
+                name: "P1ID",
+                table: "Measurements",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "P1ID",
+                table: "Measurements");
+
+            migrationBuilder.RenameColumn(
+                name: "P1ID",
+                table: "Households",
+                newName: "RegionCode");
+        }
+    }
+}
